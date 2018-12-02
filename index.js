92d2bc15f523d9f39889c91816a0a805245571bd
Alex <cot15011@byui.edu>
2018-12-02 15:21:40 -0800

    more functionality - week 11

--- index.js
+++ index.js
@@ -26,11 +26,11 @@ app.get('/', async (req, res) => {
       res.send("Error " + err);
     }
   })
-app.get('/read', function(req,res) {
+// app.get('/read', function(req,res) {
 
-});
-app.get('/transcribe', function(req,res) {
+// });
+// app.get('/transcribe', function(req,res) {
 
-});
+// });
 
-  //.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
