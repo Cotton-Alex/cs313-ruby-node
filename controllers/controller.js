963524d88667fc5b71d55465372a510c4e117d27
Alex <cot15011@byui.edu>
2018-12-08 14:06:41 -0800

    added hide read/transcribe sections

--- controllers/controller.js
+++ controllers/controller.js
@@ -3,28 +3,28 @@ const model = require("../models/model.js");
 function read(req,res) {
 	console.log("controller.read() called");
 	model.displayRead();
-	var results = model.getByImageNameRead();
+	//var results = model.getByImageNameRead();
 
-	res.json(results);
+	//res.json(results);
 };
 
 function getTranscribe(req,res) {
 	console.log("controller.getTranscribe() called");
 	//hide read & show getTranscribe
 	model.displayTranscribe();
-	var id = req.query.id;
-	var results = model.getByImageNameTran(id);
+	//var id = req.query.id;
+	//var results = model.getByImageNameTran(id);
 
-	res.json(results);
+	//res.json(results);
 };
 
 function postTranscribe(req,res) {
 	console.log("controller.postTranscribe() called");
 	model.displayTranscribe();
-	var id = req.query.id;
-	var results = model.insertEntry(id);
+	//var id = req.query.id;
+	//var results = model.insertEntry(id);
 
-	res.json(results);
+	//res.json(results);
 };
 
 module.exports = {
