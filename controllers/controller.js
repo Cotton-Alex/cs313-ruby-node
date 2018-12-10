e415554da62dbab5cd9a8f446a14793b3b94b932
Alex <cot15011@byui.edu>
2018-12-09 17:48:08 -0800

    mvc fix

--- controllers/controller.js
+++ controllers/controller.js
@@ -2,6 +2,12 @@ const model = require("../models/model.js");
 
 function read(req, res) {
     console.log("controller.read() called");
+    var journal_name = "1946-1950";
+    var journal_month = "01";
+    var journal_day = "07";
+    var image_file_name = (journal_month + "-" + journal_month + "-" + journal_day + ".jpg")
+    console.log("image_file_name = " + image_file_name);
+    
     model.displayRead();
     //document.getElementById('section_read').style.display = 'block';
     //document.getElementById('section_transcribe').style.display = 'none';
